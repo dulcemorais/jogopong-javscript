9cf380bd545920e42e95891081dd837cca07bbce
dulcemorais_157 <106695819+dulcemorais@users.noreply.github.com>
2022-06-15 14:55:35 -0300

    adicionando arquivos do jogo

---
+++ sketch.js
@@ -0,0 +1,27 @@
+let xbolinha = 300;
+let ybolinha = 200;
+let diametro = 15;
+
+let velocidadexbolinha = 6;
+let velocidadeybolinha = 6;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  circle(xbolinha,ybolinha, diametro);
+  xbolinha += velocidadexbolinha;
+  ybolinha += velocidadeybolinha;
+  
+  if  (xbolinha > width || 
+    xbolinha < 0){
+    velocidadexbolinha *= -1;
+  }
+ if (ybolinha > height||
+   ybolinha < 0 )
+   velocidadeybolinha *= -1;
+ 
+    
+}
